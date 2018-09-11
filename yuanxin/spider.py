a32a1d1fb1bfb73d073d9bc7c9bdb4bc1bd10d55
452MJ <447180005@qq.com>
2018-09-11 12:01:14 +0800

    添加源鑫爬虫

--- yuanxin/spider.py
+++ yuanxin/spider.py
@@ -6,6 +6,7 @@ import urllib
 
 from yuanxin.spider_category import get_category
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
@@ -29,9 +30,9 @@ def get_html(url):
 
 
 def write_csv(list):
-    columns = ['一级分类', '二级分类', '药品名', '产品规格', '生产厂家', '零售参考价', '采购价']
+    columns = ['药品分类', '商品名称', '包装规格', '生产厂家', '批准文号', '采购价', '折扣价', '销量']
     csvfile = pd.DataFrame(columns=columns, data=list)  # 打开方式还可以使用file对象
-    csvfile.to_csv('云药库.csv', index=False, encoding='GBK')
+    csvfile.to_csv('源鑫.csv', index=False, encoding='GBK')
 
 
 def get_total_page(url):
@@ -55,22 +56,31 @@ def get_item_list(url, item_list, category):
     soup = BeautifulSoup(html, "html.parser")
     div_array = soup.findAll('div', {'class': 'single_pro'})
     item_link_list = []
+    sale_array = []
     for i in range(len(div_array)):
         try:
             a = div_array[i].find('a')
             item_link = a['href']
             item_link_list.append(item_link)
+            span = div_array[i].find('span', {'class': 'sales'})
+            if (span != None):
+                span = span.text
+                span = span[2:len(span)]
+                span = span[:-1]
+                sale_array.append(span)
+            else:
+                sale_array.append(0)
         except BaseException as e:
             print(e)
     for i in range(len(item_link_list)):
         try:
-            item_list.append(get_item_detail('https://yx.5kjr.cn' + item_list[i]))
+            item_list.append(get_item_detail('https://yx.5kjr.cn' + item_link_list[i], category, sale_array[i]))
         except BaseException as e:
             print(e)
     return item_list
 
 
-def get_item_detail(url):
+def get_item_detail(url, category, sale):
     html = get_html(url)
     soup = BeautifulSoup(html, "html.parser")
     # 采购价
@@ -87,22 +97,32 @@ def get_item_detail(url):
     item_name = item_name[1].text
     # 包装规格
     item_standard = tr_array[1].findAll('td')
-    item_standard = item_name[1].text
+    item_standard = item_standard[1].text
     # 生产厂家
     item_supplier = tr_array[2].findAll('td')
-    item_supplier = item_name[1].text
+    item_supplier = item_supplier[1].text
     # 批准文号
     item_number = tr_array[3].findAll('td')
-    item_number = item_name[1].text
-    return {
-        'refer_price': refer_price,
-        'activity_price': activity_price,
-        'item_name': item_name,
-        'item_standard': item_standard,
-        'item_supplier': item_supplier,
-        'item_number': item_number,
-    }
-
+    item_number = item_number[1].text
+    return [
+        category['name'],
+        item_name,
+        item_standard,
+        item_supplier,
+        item_number,
+        refer_price,
+        activity_price,
+        sale
+    ]
+    # return {
+    #     'category': category,
+    #     'refer_price': refer_price,
+    #     'activity_price': activity_price,
+    #     'item_name': item_name,
+    #     'item_standard': item_standard,
+    #     'item_supplier': item_supplier,
+    #     'item_number': item_number,
+    # }
 
 
 category_array = get_category()
@@ -120,5 +140,5 @@ for category_index in range(len(category_array)):
         page = i + 1
         final_url = url + '?page=' + str(page)
         get_item_list(final_url, records, category)
-    print('{0}/{1}'.format(category_index + 1, len(category_array)))
+        print((category_index + 1), '/', len(category_array), '  ', (i + 1), "/", total)
 write_csv(records)
